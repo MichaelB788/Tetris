a9e108c373ef79a33ea7e9ca4ac5591220e49826
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-08-17 21:04:12 -0400

    Implementation of Render and Window namespaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,54 @@
-
 #include <iostream>
 #include <SDL2/SDL.h>
+
 #include "Components/Window.h"
+#include "Components/Render.h"
 
-int main (int argc, char **argv){
+// Access SDL systems
+struct SDL_Systems
+{
+    SDL_Window* win;
+    SDL_Renderer* ren;
+};
 
-    // SDL_Color white = {255, 255, 255};
-    SDL_Window *win = NULL;
+void terminate()
+{
+    struct SDL_Systems sdl;
 
-    Window::init(win);
-    Window::open(win);
-    Window::close(win);
+    SDL_DestroyWindow(sdl.win);
+    sdl.win = NULL;
 
+    SDL_DestroyRenderer(sdl.ren);
+    sdl.ren = NULL;
+}
+
+int main (int argc, char **argv)
+{
+    struct SDL_Systems sdl;
+
+    sdl.win = NULL;
+    sdl.ren = NULL;
+
+    // Handle window
+    sdl.win = Window::createWindow();  
+    if ( Window::isNull(sdl.win) )
+    { 
+        terminate();
+        return -1;
+    }
+
+    // Handle renderer
+    sdl.ren = SDL_CreateRenderer(sdl.win, -1, 0);
+    if ( Render::isNull(sdl.ren) )
+    { 
+        terminate();
+        return -1;
+    }
+
+    // Test
+    Render::drawTetromino();
+    
+    SDL_Delay(10000);
+    terminate();
     return 0;
-
 }

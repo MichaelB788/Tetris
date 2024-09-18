338e7a0842e6b2bd78744d364ec6e10aac9b7bdc
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-09-17 23:36:03 -0400

    TODO: get proper block rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 // SDL2 Library
 #include <SDL2/SDL.h>
+#include <vector>
 
 // SDL Systems
-#include "src/Components/SDLComponents.h"
+#include "Components/Window.h"
+#include "Components/Render.h"
+#include "Components/SDLComponents.h"
+
+#include "Game/Block.h"
 
 int main (int argc, char **argv)
 {
-    // Initialize SDL
-    if (!SDL_Components::initialize()) return 1;
+    // Initialize all SDL subsystems
+    if (!SDL_Components::initialize()) return -1;
 
     // Window and Renderer subsystems
     SDL_Window* win = SDL_Components::getWindow();
     SDL_Renderer* ren = SDL_Components::getRenderer();
+
+    Block test = Block('T');  
+    Render::drawBlock(test, ren);
 
     SDL_Delay(10000);
     SDL_Components::terminate();

475fdfbc1720848c859139b0b668cb05a814e295
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-10-17 21:03:50 -0400

    diff: modularized main, it only needs to instantiate a Tetris class

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,10 @@
-// SDL2 Library
 #include <SDL2/SDL.h>
 
-// SDL API
-#include "API/SDLUtils.hpp"
-
-// Game Objects
-#include "Game/Piece.hpp"
-#include "Game/Board.hpp"
-
-// Custom Components
-#include "Components/Graphics.hpp"
-#include "Components/EventHandler.hpp"
+#include "Game/Tetris.hpp"
 
 int main (int argc, char **argv)
 {
-    SDLUtils::initEverything();
-
-    bool gameIsRunning = true;
-    SDL_Event event;
-
-    const char piecetypes[7] = {'I','O','T','S','Z','L','J'};
-
-    while (gameIsRunning)
-    {
-        // Handle the events
-        EventHandler::process(event, gameIsRunning);
-
-        // Clear the screen
-        SDL_RenderClear(SDLUtils::renderer.pSDL_Renderer);
-
-        // Update game
-
-        // Draw our new frame
-        Graphics::renderFrame(Board::arrayOfArrays, SDLUtils::renderer.pSDL_Renderer);
-
-        // Show our new frame
-        SDL_RenderPresent(SDLUtils::renderer.pSDL_Renderer); 
-    }
-    
+    new Tetris();
     SDL_Quit();
-
     return 0;
 }

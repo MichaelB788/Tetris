9d5e5eb3309263fc8e0818a36ad0b61c0c33cc0f
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-10-19 00:57:57 -0400

    diff: demodularized main, opted out of extern/global classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <SDL2/SDL.h>
 
 #include "Game/Tetris.hpp"
+#include "API/GameCore.hpp"
+#include "Components/Graphics.hpp"
 
 int main (int argc, char **argv)
 {
-    new Tetris();
+    GameCore core;
+
+    GraphicsModule graphics = GraphicsModule(core.m_Renderer);
+
+    Tetris tetris;
+
+    tetris.runGame(graphics);
+
+    core.terminate();
+
     SDL_Quit();
+
     return 0;
 }

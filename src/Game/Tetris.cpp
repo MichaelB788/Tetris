d9985b4edcf2806f9344b7ae1ac5ca2efde5c30b
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-10-20 17:14:25 -0400

    diff: testing piece behavior

--- src/Game/Tetris.cpp
+++ src/Game/Tetris.cpp
@@ -1,21 +1,21 @@
 #include <SDL2/SDL.h>
+#include <cstdlib>
 
 #include "Tetris.hpp"
 
 #include "../Components/Graphics.hpp"
 #include "../Components/EventHandler.hpp"
 
-#include "Objects/Board.hpp"
-#include "Objects/Piece.hpp"
-
-char piecetypes[7] = {'I','O','T','S','Z','L','J'};
+#include "Objects/TetrisBoard.hpp"
 
 void Tetris::runGame(GraphicsModule gm)
 {
+    TetrisBoard::Piece p;
+
     while (gameIsRunning)
     {
         EventHandler::processInput(event, gameIsRunning);
 
-        gm.clearAndPresentFrame(board);
+        gm.clearAndPresentFrame();
     }
 }
--- src/Game/Tetris.hpp
+++ src/Game/Tetris.hpp
@@ -3,19 +3,15 @@
 
 #include <SDL2/SDL.h>
 #include "../Components/Graphics.hpp"
-#include "Objects/Board.hpp"
-#include "Objects/Piece.hpp"
 
 typedef class Tetris
 {
 private:
     SDL_Event event;
     bool gameIsRunning = true;
-    Board board;
 
 public:
     Tetris() = default;
-
     void runGame(GraphicsModule gm);
 } Tetris;
 

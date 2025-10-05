7fa02959f4eab0006183f11acd29ffcdd47f43bd
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-10-05 03:24:27 -0400

    TODO: give tetromino a shape upon creation

--- include/core/tetromino.hpp
+++ include/core/tetromino.hpp
@@ -1,20 +1,21 @@
 #ifndef TETROMINO_H
 #define TETROMINO_H
-#include <cstdint>
 #include <array>
+#include "util/data-structures.hpp"
 
 /* *
- * @breif Tetromino structs contain data, namely: coordinates (in index form), color, and type
+ * @breif Tetromino structs contain data, namely: color and an array of tiles representing the shape.
  * */
 struct Tetromino {
 	enum class Type : uint8_t { I, O, T, Z, S, J, L };
-	enum class Role : uint8_t { NONE = 0, ACTIVE, GROUNDED, GHOST };
 	enum class Color : uint8_t { LIGHT_BLUE, ORANGE, BLUE, GREEN, RED, PURPLE, YELLOW, WHITE };
+	enum class Role : uint8_t { NONE = 0, ACTIVE, GROUNDED, GHOST };
 
-	Type type;
-	Role role;
-	Color color;
-	std::array<uint8_t, 4> indexes;
+	Type m_type;
+	Color m_color;
+	Role m_role;
+	std::array<Vector2, 4> m_position;
+	Tetromino(Type type, Color color, Role role) : m_type(type), m_color(color), m_role(role) {};
 };
 
 #endif

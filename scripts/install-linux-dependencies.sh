8dedc02ac6194011ebe73a0f00addd77f3ff8a23
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-10-18 09:22:16 -0400

    Updated script to install linux dependencies (#43)
    Tested and working on Linux Debian

--- scripts/install-linux-dependencies.sh
+++ scripts/install-linux-dependencies.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
 set -e
 
-if [ -f /etc/os-release ]; then
-  . /etc/os-release
-  OS_FAMILY="${ID_LIKE:-$ID}"
-  case "$OS_FAMILY" in
-    *debian*) 
-      INSTALL="sudo apt-get install -y"
-      SDL=(libsdl2-dev libsdl2-2.0-0)
-      ;;
-    *arch*)
-      INSTALL="sudo pacman -S --noconfirm"
-      SDL=sdl2
-      ;;
-    *fedora*)
-      INSTALL="sudo dnf install -y"
-      SDL=SDL2-devel
-      ;;
-    *)
-      echo "This script isn't supported with this Linux distro: $ID"
-      exit 1
-  esac
-else
-  echo "Cannot determine the operating system using /etc/os-release. This script may not be compatible."
-  exit 1
-fi
+set_os_specific_variables() {
+	if [ -f /etc/os-release ]; then
+		. /etc/os-release
+		OS_FAMILY="${ID_LIKE:-$ID}"
+		case "$OS_FAMILY" in
+			*debian*) 
+				SDL_DEP=(libsdl2-dev libsdl2-2.0-0 libsdl2-ttf-dev libsdl2-ttf-2.0-0)
+				CATCH_2=catch2
+				;;
+			*arch*)
+				SDL_DEP=(sdl2 sdl2_ttf)
+				CATCH_2=catch2
+				;;
+			*fedora*)
+				SDL_DEP=(SDL2-devel SDL2_ttf-devel)
+				CATCH_2=catch2-devel
+				;;
+			*)
+				echo "This script isn't supported with this Linux distro: $ID"
+				exit 1
+				;;
+		esac
+	else
+		echo "Cannot determine the operating system using /etc/os-release. This script may not be compatible."
+		exit 1
+	fi
+}
 
-PROJECT_DEPENDENCIES=(sdl)
+install_package() {
+	if command -v apt-get &> /dev/null; then
+		for arg in "$@"; do
+			sudo apt-get install $arg
+		done
+	elif command -v pacman &> /dev/null; then
+		for arg in "$@"; do
+			sudo pacman -Ss $arg
+		done
+	elif command -v dnf &> /dev/null; then
+		for arg in "$@"; do
+			sudo dnf $arg
+		done
+	fi
+}
+
+install_all_packages() {
+	install_package "${SDL_DEP[@]}"
+	install_package $CATCH_2
+}
+
+set_os_specific_variables
+install_all_packages

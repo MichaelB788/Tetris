4dee3a13574f47973d654c17f7dbd2a51aee073f
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-10-26 02:12:47 -0400

    fix: Updated doxy comments

--- include/core/tetromino.hpp
+++ include/core/tetromino.hpp
@@ -6,18 +6,15 @@
 
 enum class TetrominoType : uint8_t { I, O, T, Z, S, J, L };
 
-/// @brief A piece of a certain type. Contains information regarding it's shape.
+/// @brief Holds data about the Tetromino's position and type, and can manipulate this data through movement and rotation.
 class Tetromino {
 private:
-	using Coordinates = std::array<Vector2, 4>;
   TetrominoType m_type;
-	Coordinates m_coordinates;
+	std::array<Vector2, 4> m_coordinates;
 
 public:
-  Tetromino(TetrominoType type, Vector2 pivot);
 	constexpr TetrominoType type() { return m_type; }
-	/// @brief Returns the Tetromino's coordinates, which is an array of Vector2 with a size of 4
-	constexpr Coordinates& coordinates() { return m_coordinates; }
+	constexpr std::array<Vector2, 4>& coordinates() { return m_coordinates; }
 
   constexpr void moveLeft() { for (auto& c : m_coordinates) c.x--; }
   constexpr void moveRight() { for (auto& c : m_coordinates) c.x++; }

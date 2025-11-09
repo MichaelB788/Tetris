b83ce054d6f144578e59dd4bdddcbf6db4421e81
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-11-09 16:41:41 -0500

    diff: slightly improved readability

--- src/core/super-rotation-system.cpp
+++ src/core/super-rotation-system.cpp
@@ -1,12 +1,13 @@
 #include "core/super-rotation-system.hpp"
 
 SuperRotationSystem::SuperRotationSystem(Tetromino& tetromino) : m_tetromino(tetromino) {
+	using v2 = Vector2;
 	if (m_tetromino.type() == TetrominoType::I) {
 		m_offsetData = {{
-			{ Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), },
-			{ Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), },
-			{ Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), },
-			{ Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), Vector2(0, 0), },
+			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
+			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
+			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
+			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
 		}};
 	}
 }

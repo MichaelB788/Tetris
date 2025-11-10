519ad4bee757c7cba81e252bf3ca58dd3be1b04c
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-11-09 19:29:09 -0500

    diff: Implemented offsets

--- include/core/super-rotation-system.hpp
+++ include/core/super-rotation-system.hpp
@@ -1,17 +1,37 @@
 #ifndef SUPER_ROTATION_SYSTEM_H
 #define SUPER_ROTATION_SYSTEM_H
+#include "core/matrix.hpp"
 #include "core/tetromino.hpp"
 #include "util/vector2.hpp"
 #include <array>
 
 class SuperRotationSystem {
 public:
-	SuperRotationSystem(Tetromino& tetromino);
+	using v2 = Vector2;
+	SuperRotationSystem(Tetromino& tetromino, Matrix& matrix)
+		: r_tetromino(tetromino),
+			r_matrix(matrix),
+			m_offsetDataStandard({{
+					{ v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0), },
+					{ v2( 0,  0), v2( 1,  0), v2( 1,  1), v2( 0, -2), v2( 1, -2), },
+					{ v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0), },
+					{ v2( 0,  0), v2(-1,  0), v2(-1,  1), v2( 0, -2), v2(-1, -2), },
+					}}),
+			m_offsetDataI({{
+					{ v2( 0,  0), v2(-1,  0), v2( 2,  0), v2(-1,  0), v2( 2,  0), },
+					{ v2(-1,  0), v2( 0,  0), v2( 0,  0), v2( 0, -1), v2( 1, +2), },
+					{ v2(-1, -1), v2( 1, -1), v2(-2, -1), v2( 1,  0), v2(-2,  0), },
+					{ v2( 0,  1), v2(-1,  0), v2(-1,  1), v2( 0, -2), v2( 0,  0), },
+					}})
+	{};
+
 	bool performRotationTests();
 
 private:
-	std::array<std::array<Vector2, 5>, 4> m_offsetData;
-	Tetromino& m_tetromino; 
+	std::array<std::array<Vector2, 5>, 4> m_offsetDataStandard;
+	std::array<std::array<Vector2, 5>, 4> m_offsetDataI;
+	Tetromino& r_tetromino; 
+	Matrix& r_matrix;
 };
 
 #endif
--- src/core/super-rotation-system.cpp
+++ src/core/super-rotation-system.cpp
@@ -1,13 +1,5 @@
 #include "core/super-rotation-system.hpp"
 
-SuperRotationSystem::SuperRotationSystem(Tetromino& tetromino) : m_tetromino(tetromino) {
-	using v2 = Vector2;
-	if (m_tetromino.type() == TetrominoType::I) {
-		m_offsetData = {{
-			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
-			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
-			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
-			{ v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), v2(0, 0), },
-		}};
-	}
+bool performRotationTests() {
+
 }

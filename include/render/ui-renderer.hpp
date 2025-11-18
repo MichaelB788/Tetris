f0b76c8dc4fd7b518d9427b86938ef1f57375eb5
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-11-18 18:58:25 -0500

    TODO: Implement a TetrominoRenderer, figure out a proper horizontal offset

--- include/render/ui-renderer.hpp
+++ include/render/ui-renderer.hpp
@@ -3,19 +3,23 @@
 #include "util/renderer.hpp"
 #include "util/text.hpp"
 #include "util/geometry.hpp"
+#include "render/matrix-renderer.hpp"
 
 class UIRenderer {
 public:
-	UIRenderer() : storedText("Stored:"), nextText("Next:") {};
+	UIRenderer(SDL_Renderer* renderer)
+		: storedText("Stored", renderer),
+			nextText("Next", renderer),
+			offset(0, 40)
+	{};
 
-	void createTextTexture(SDL_Renderer* renderer);
-
-	void renderText(const Renderer& renderer, const Dimension2D window) const;
+	void renderText(const Renderer& renderer, const MatrixRenderer& matrixRenderer);
+	void renderStoredAndNext(const Tetromino& stored, const Tetromino& next);
 
 private:
-	// BUG: Since these are stack allocated, TTF_Init initializes after these objects.
 	Text storedText;
 	Text nextText;
+	Vector2 offset;
 };
 
 #endif
--- src/render/ui-renderer.cpp
+++ src/render/ui-renderer.cpp
@@ -1,13 +1,13 @@
 #include "render/ui-renderer.hpp"
 
-void UIRenderer::createTextTexture(SDL_Renderer* renderer) {
-	storedText.createTextTexture(renderer);
-	nextText.createTextTexture(renderer);
+void UIRenderer::renderText(const Renderer& renderer, const MatrixRenderer& matrixRenderer) {
+	if ( storedText.isInitialized() && nextText.isInitialized() ) {
+		offset.x = matrixRenderer.getMatrixEnd();
+		storedText.render(offset.x, offset.y, renderer);
+		nextText.render(offset.x, offset.y + 200, renderer);
+	}
 }
 
-void UIRenderer::renderText(const Renderer& renderer, const Dimension2D windowDimension) const {
-	if ( storedText.isInitialized() && nextText.isInitialized() ) {
-		storedText.render(0, 0, renderer);
-		nextText.render(0, 0, renderer);
-	}
+void UIRenderer::renderStoredAndNext(const Tetromino& stored, const Tetromino& next) {
+	// TODO: Tetromino Renderer
 }

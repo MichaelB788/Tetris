74e7bd15670921627593e9679745bbb50ab4266b
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-11-21 21:20:31 -0500

    new: Implementing a circular, fixed size queue for Tetromino Queue

--- include/core/tetromino-queue.hpp
+++ include/core/tetromino-queue.hpp
@@ -1,6 +1,7 @@
 #ifndef TETROMINO_QUEUE_H
 #define TETROMINO_QUEUE_H
 #include "core/tetromino.hpp"
+#include "util/circular-array.hpp"
 #include <random>
 
 class TetrominoQueue {
@@ -15,18 +16,12 @@ public:
 
 	void reset();
 
-	// Iterator
-	using const_iterator = std::array<Tetromino, 4>::const_iterator;
-
-	const_iterator begin() const { return nextQueue.begin(); }
-
 private:
 	void fillNextQueue(std::mt19937& gen) {
 		for (auto& tetromino : nextQueue) tetromino = Tetromino(Tetromino::getRandomType(gen));
 	};
 
-	uint8_t index = 0;
-	std::array<Tetromino, 4> nextQueue;
+	CircularArray<Tetromino, 4> nextQueue;
 
 	std::mt19937& gen;
 	bool currentIsHeld = false;

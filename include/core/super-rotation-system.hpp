627a6b3480fc56c71fcb8dce549bc70c0cd34e2f
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-11-25 18:46:07 -0500

    diff: Made performOffsetTests a static member function

--- include/core/super-rotation-system.hpp
+++ include/core/super-rotation-system.hpp
@@ -10,8 +10,9 @@ public:
 	static void rotateTetromino(Tetromino& actor, Matrix& scene, Direction::Rotation direction);
 
 private:
-	using v2 = Vector2;
+	static bool performOffsetTests(const std::array<std::array<Vector2, 5>, 4>& offsets, Tetromino& actor, const Matrix& scene, const uint8_t stateBefore, const uint8_t stateAfter);
 
+	using v2 = Vector2;
 	static constexpr std::array<std::array<Vector2, 5>, 4> offsetDataStandard {{
 		{ v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0), v2( 0,  0) },
 		{ v2( 0,  0), v2( 1,  0), v2( 1,  1), v2( 0, -2), v2( 1, -2) },
@@ -20,10 +21,10 @@ private:
 	}};
 
 	static constexpr std::array<std::array<Vector2, 5>, 4> offsetDataI {{
-		{ v2( 0,  0), v2(-1,  0), v2( 2,  0), v2(-1,  0), v2( 2,  0), },
-		{ v2(-1,  0), v2( 0,  0), v2( 0,  0), v2( 0, -1), v2( 1, +2), },
-		{ v2(-1, -1), v2( 1, -1), v2(-2, -1), v2( 1,  0), v2(-2,  0), },
-		{ v2( 0, -1), v2( 0, -1), v2( 0, -1), v2( 0,  1), v2( 0, -2), }
+		{ v2( 0,  0), v2(-1,  0), v2( 2,  0), v2(-1,  0), v2( 2,  0) },
+		{ v2(-1,  0), v2( 0,  0), v2( 0,  0), v2( 0, -1), v2( 1, +2) },
+		{ v2(-1, -1), v2( 1, -1), v2(-2, -1), v2( 1,  0), v2(-2,  0) },
+		{ v2( 0, -1), v2( 0, -1), v2( 0, -1), v2( 0,  1), v2( 0, -2) }
 	}};
 };
 
--- src/core/super-rotation-system.cpp
+++ src/core/super-rotation-system.cpp
@@ -1,19 +1,5 @@
 #include "core/super-rotation-system.hpp"
 
-namespace {
-	bool performOffsetTests(const std::array<std::array<Vector2, 5>, 4>& offsets, Tetromino& actor, const Matrix& scene, const uint8_t stateBefore, const uint8_t stateAfter) {
-		for (int i = 0; i < offsets.size(); i++) {
-			Vector2 translation = offsets[stateBefore][i] - offsets[stateAfter][i];
-			actor.shift(translation);
-
-			if (!scene.doesTetrominoCollideImpermeable(actor)) return true;
-
-			actor.shift(-translation);
-		}
-		return false;
-	}
-}
-
 void SuperRotationSystem::rotateTetromino(Tetromino& actor, Matrix& scene, Direction::Rotation direction) {
 	scene.removeTetromino(actor);
 	uint8_t originalRotationState = actor.getRotationState();
@@ -30,4 +16,16 @@ void SuperRotationSystem::rotateTetromino(Tetromino& actor, Matrix& scene, Direc
 		actor.rotate(Direction::getCounterRotation(direction));
 	}
 	scene.placeTetromino(actor);
+}
+
+bool SuperRotationSystem::performOffsetTests(const std::array<std::array<Vector2, 5>, 4>& offsets, Tetromino& actor, const Matrix& scene, const uint8_t stateBefore, const uint8_t stateAfter) {
+	for (int i = 0; i < offsets.size(); i++) {
+		Vector2 translation = offsets[stateBefore][i] - offsets[stateAfter][i];
+		actor.shift(translation);
+
+		if (!scene.doesTetrominoCollideImpermeable(actor)) return true;
+
+		actor.shift(-translation);
+	}
+	return false;
 }

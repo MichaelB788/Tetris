ddf8130b28560b6ce08a24045502e468c5033c6a
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-03-28 16:42:27 -0400

    Using Circular buffer for NextQueue

--- include/NextQueue.hpp
+++ include/NextQueue.hpp
@@ -1,58 +1,18 @@
 #pragma once
+#include "CircularBuffer.hpp"
 #include "Tetromino.hpp"
-#include <array>
 
 class NextQueue {
 public:
-  NextQueue();
+  NextQueue() { shuffle(); }
 
-  Tetromino pop_next();
+  [[nodiscard]] Tetromino::Type pop();
 
-  struct const_iterator {
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type = std::ptrdiff_t;
-
-    const_iterator(const Tetromino *base, std::size_t indx, std::size_t size)
-        : base_(base), indx_(indx), size_(size) {}
-
-    const Tetromino &operator*() const { return base_[indx_]; }
-    const Tetromino *operator->() const { return &base_[indx_]; }
-
-    const_iterator &operator++() {
-      indx_ = (indx_ + 1) % size_;
-      return *this;
-    }
-    const_iterator operator++(int) {
-      const_iterator tmp = *this;
-      operator++();
-      return tmp;
-    }
-
-    friend bool operator==(const_iterator &a, const_iterator &b) {
-      return a.base_ == b.base_ && a.indx_ == b.indx_ && a.size_ == b.size_;
-    }
-    friend bool operator!=(const_iterator &a, const_iterator &b) {
-      return !(a == b);
-    }
-
-  private:
-    const Tetromino *base_;
-    std::size_t indx_;
-    std::size_t size_;
-  };
-
-  const_iterator begin() const {
-    return const_iterator(queue_.data(), read, queue_.size());
-  }
-
-  const_iterator end() const {
-    return const_iterator(queue_.data(), write, queue_.size());
-  }
+  void shuffle();
 
 private:
-  void push_non_repeating();
+  void push_random();
 
-  std::array<Tetromino, 5> queue_{};
-
-  std::size_t read = 0, write = queue_.size() - 1;
+private:
+  CircularBuffer<Tetromino::Type, 4> queue_{};
 };
--- src/NextQueue.cpp
+++ src/NextQueue.cpp
@@ -1,31 +1,22 @@
 #include "NextQueue.hpp"
-#include "Point.hpp"
 #include "Tetromino.hpp"
 
-NextQueue::NextQueue() {
-  for (std::size_t i = 0; i < queue_.size(); ++i)
-    push_non_repeating();
+void NextQueue::shuffle() {
+  queue_.clear();
+  while (queue_.size() < queue_.max_size())
+    push_random();
 }
 
-Tetromino NextQueue::pop_next() {
-  Tetromino next = queue_[read];
-  next.set_pos(Point(4, 2));
-  read = (read + 1) % queue_.size();
-  push_non_repeating();
-  return next;
+Tetromino::Type NextQueue::pop() {
+  Tetromino::Type ret = queue_.pop();
+  push_random();
+  return ret;
 }
 
-void NextQueue::push_non_repeating() {
-  TetrominoType random = Tetromino::get_random_type();
-
-  if (read != write) {
-    const std::size_t prev = write == 0 ? queue_.size() - 1 : write - 1;
-
-    do {
-      random = Tetromino::get_random_type();
-    } while (random == queue_[prev].type_());
-  }
-
-  queue_[write] = Tetromino(random, {.x = 0, .y = 0});
-  write = (write + 1) % queue_.size();
+void NextQueue::push_random() {
+  Tetromino::Type random;
+  do {
+    random = Tetromino::random_type();
+  } while (random == queue_.back());
+  queue_.push(random);
 }

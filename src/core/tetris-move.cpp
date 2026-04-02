9f7fce706f057fdcbb49b08202a47930ecac334d
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-04-02 12:29:23 -0400

    Fixed internal I piece rotation states and SRS kick offset

--- src/core/tetris-move.cpp
+++ src/core/tetris-move.cpp
@@ -35,28 +35,28 @@ constexpr Point STANDARD_PIECE_OFFSETS[Tetromino::NUM_ROTATION][NUM_TRIALS]{
 
 constexpr Point I_PIECE_OFFSETS[Tetromino::NUM_ROTATION][NUM_TRIALS]{
     {{0, 0}, {-1, 0}, {2, 0}, {-1, 0}, {2, 0}},     // R0
-    {{-1, 0}, {0, 0}, {0, 0}, {0, -1}, {1, 2}},     // R90
+    {{-1, 0}, {0, 0}, {0, 0}, {0, -1}, {0, 2}},     // R90
     {{-1, -1}, {1, -1}, {-2, -1}, {1, 0}, {-2, 0}}, // R180
     {{0, -1}, {0, -1}, {0, -1}, {0, 1}, {0, -2}}};  // R270
 
 using RotationFunc = void (Tetromino::*)();
 
 void srs_rotation(Tetromino &piece, const Matrix &matrix, RotationFunc rotate) {
-  Tetromino rotated = piece;
-  (rotated.*rotate)();
-
   Tetromino::Rotation prev = piece.rotation();
-  Tetromino::Rotation curr = rotated.rotation();
+  (piece.*rotate)();
+  Tetromino::Rotation curr = piece.rotation();
 
   const auto &offsets =
       piece.type() == Tetromino::I ? I_PIECE_OFFSETS : STANDARD_PIECE_OFFSETS;
   for (size_t i = 0; i < NUM_TRIALS; ++i) {
     Point kick = offsets[prev][i] - offsets[curr][i];
-    if (matrix.is_move_valid(rotated.test_shift(kick))) {
-      piece = rotated;
+    if (matrix.is_move_valid(piece.test_shift(kick))) {
+      piece.shift(kick);
       return;
     }
   }
+
+  piece.set_rotation(prev);
 }
 } // namespace
 

3531150874a8ae35b9bf1498c3f902de99825b8a
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-08 21:34:58 -0400

    Using cmake's target_compile_definition to resolve project root path

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,9 @@
 #include <filesystem>
 #include <iostream>
 
-#ifdef _WIN32
-#include <windows.h>
-#elif defined(__APPLE__)
-#include <mach-o/dyld.h>
-#elif defined(__linux__)
-#include <unistd.h>
-#endif
-
-std::filesystem::path get_binary_path() {
-#ifdef _WIN32
-  wchar_t path[MAX_PATH];
-  GetModuleFileNameW(nullptr, path, MAX_PATH);
-  return std::filesystem::path(path);
-#elif defined(__APPLE__)
-  char path[1024];
-  uint32_t size = sizeof(path);
-  _NSGetExecutablePath(path, &size);
-  return std::filesystem::canonical(path);
-#elif defined(__linux__)
-  return std::filesystem::canonical("/proc/self/exe");
-#endif
-}
-
 int main() {
-  std::filesystem::path project_root = get_binary_path();
-  for (int i = 0; i < 3; ++i)
-    project_root = project_root.parent_path();
-
   try {
+    const std::filesystem::path project_root = PROJECT_ROOT;
     if (SDL_Init(SDL_INIT_VIDEO) && TTF_Init()) {
       TetrisApp(
           {.controls = project_root / "controls.ini",

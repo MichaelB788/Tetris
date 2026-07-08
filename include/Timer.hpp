d80e31d27d01fb5b2305002df451615e2e845045
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-08 01:38:31 -0400

    Swapped position of lambda and delta arguments around

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -8,10 +8,10 @@ public:
   void operator+=(std::chrono::nanoseconds delta) { accumulator_ += delta; }
 
   template <typename Fn>
-  void invoke_when_elapsed(std::chrono::nanoseconds delta, Fn &&function);
+  void invoke_when_elapsed(Fn &&function, std::chrono::nanoseconds delta);
 
   template <typename Fn>
-  void invoke_periodically(std::chrono::nanoseconds delta, Fn &&function);
+  void invoke_periodically(Fn &&function, std::chrono::nanoseconds delta);
 
   void set_duration(std::chrono::nanoseconds duration) { duration_ = duration; }
 
@@ -36,8 +36,8 @@ private:
 };
 
 template <typename Fn>
-inline void Timer::invoke_when_elapsed(std::chrono::nanoseconds delta,
-                                       Fn &&function) {
+inline void Timer::invoke_when_elapsed(Fn &&function,
+                                       std::chrono::nanoseconds delta) {
   accumulator_ += delta;
   if (has_elapsed()) {
     function();
@@ -45,8 +45,8 @@ inline void Timer::invoke_when_elapsed(std::chrono::nanoseconds delta,
 }
 
 template <typename Fn>
-inline void Timer::invoke_periodically(std::chrono::nanoseconds delta,
-                                       Fn &&function) {
+inline void Timer::invoke_periodically(Fn &&function,
+                                       std::chrono::nanoseconds delta) {
   accumulator_ += delta;
   while (has_elapsed()) {
     accumulator_ -= duration_;

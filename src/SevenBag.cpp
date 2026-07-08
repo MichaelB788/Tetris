b3839c125fafdc0f880a2e9c75a51376f484a7c4
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-08 16:12:25 -0400

    Unused include, using std::ranges::shuffle

--- src/SevenBag.cpp
+++ src/SevenBag.cpp
@@ -1,17 +1,16 @@
 #include "SevenBag.hpp"
 #include "Tetromino.hpp"
 #include <algorithm>
-#include <cstddef>
 
 SevenBag::SevenBag(std::mt19937 &rng) {
-  std::shuffle(next_bag_.begin(), next_bag_.end(), rng);
+  std::ranges::shuffle(next_bag_, rng);
   shuffle(rng);
 }
 
 void SevenBag::shuffle(std::mt19937 &rng) {
   read_ = 0;
   curr_bag_ = next_bag_;
-  std::shuffle(next_bag_.begin(), next_bag_.end(), rng);
+  std::ranges::shuffle(next_bag_, rng);
 
   if (curr_bag_.back() == next_bag_[0]) {
     // Swap with any other element, since all elements are unique
@@ -29,13 +28,12 @@ auto SevenBag::pop(std::mt19937 &rng) -> Tetromino {
   return Tetromino{.type = next_type_};
 }
 
-auto SevenBag::preview() const -> SevenBag::Preview {
-  SevenBag::Preview view{};
-  auto next_index = read_;
-  for (size_t i = 0; i < view.size(); ++i) {
-    view[i] = next_index < curr_bag_.size()
-                  ? curr_bag_[next_index++]
-                  : next_bag_[next_index++ - curr_bag_.size()];
+auto SevenBag::preview() const -> Preview {
+  Preview view{};
+  auto i = read_;
+  for (auto &type : view) {
+    type = i < curr_bag_.size() ? curr_bag_[i++]
+                                : next_bag_[i++ - curr_bag_.size()];
   }
   return view;
 }

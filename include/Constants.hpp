98d34a9219bd7cd1a283fbdf60a646faceff7ad4
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-08 14:25:45 -0400

    Dedicated file for constant integers/floats

---
+++ include/Constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+static constexpr auto PIXEL_SCALE = 32.00f;
+static constexpr auto FONT_SCALE = PIXEL_SCALE;
+
+static constexpr auto MATRIX_COLS = 10u;
+static constexpr auto MATRIX_ROWS = 24u;

e95b800cae5f9a8f73cdf40b5062293dee117e4c
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-08 01:36:40 -0400

    New FPS related classes for handling FPS and counting

---
+++ include/FPS.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "Timer.hpp"
+#include <chrono>
+
+class FPS {
+public:
+  FPS(unsigned fps) { set_fps(fps); }
+
+  void set_fps(unsigned val);
+
+  auto get_fps() const -> unsigned { return fps_; }
+
+  auto get_frame_duration() const -> std::chrono::nanoseconds {
+    return frame_duration_ns_;
+  }
+
+private:
+  unsigned fps_ = 0;
+  std::chrono::nanoseconds frame_duration_ns_{};
+};
+
+class FPS_Counter {
+public:
+  void tick(std::chrono::nanoseconds delta);
+
+  auto get_current_fps() const -> unsigned { return current_fps_; }
+
+private:
+  unsigned ticks_ = 0, current_fps_ = 0;
+  Timer delay_{std::chrono::seconds(1)};
+};
---
+++ src/FPS.cpp
@@ -0,0 +1,16 @@
+#include <FPS.hpp>
+
+void FPS::set_fps(unsigned val) {
+  fps_ = val;
+  frame_duration_ns_ = std::chrono::nanoseconds{1'000'000'000 / fps_};
+}
+
+void FPS_Counter::tick(std::chrono::nanoseconds delta) {
+  ++ticks_;
+  delay_.invoke_periodically(
+      [this] {
+        current_fps_ = ticks_;
+        ticks_ = 0;
+      },
+      delta);
+}

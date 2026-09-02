c941f0974c68c1b743b988c1e55900bd12f8dadc
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-09-02 17:47:01 -0400

    Added lambdas for easier input handling

--- src/KeyboardListener.cpp
+++ src/KeyboardListener.cpp
@@ -10,46 +10,61 @@
 auto KeyboardListener::process_input(std::chrono::nanoseconds delta)
     -> AppResult {
   auto curr_keyboard = SDL_GetKeyboardState(0);
+
+  auto is_key_pressed = [&](SDL_Scancode sc) -> bool {
+    return !prev_keyboard[sc] && curr_keyboard[sc];
+  };
+  auto is_key_held = [&](SDL_Scancode sc) -> bool {
+    return prev_keyboard[sc] && curr_keyboard[sc];
+  };
+  auto is_key_released = [&](SDL_Scancode sc) -> bool {
+    return prev_keyboard[sc] && !curr_keyboard[sc];
+  };
+
+  // Handle different input based on the current state of the game
   switch (tetris.get_state()) {
   case Tetris::State::GameOver:
-    if (curr_keyboard[SDL_SCANCODE_Y])
+    if (is_key_pressed(SDL_SCANCODE_Y))
       tetris.reset();
-    else if (curr_keyboard[SDL_SCANCODE_N])
+    else if (is_key_pressed(SDL_SCANCODE_N))
       return AppResult::Quit;
     break;
+
   case Tetris::State::Paused:
-    if (!prev_keyboard[SDL_SCANCODE_SPACE] && curr_keyboard[SDL_SCANCODE_SPACE])
+    if (is_key_pressed(SDL_SCANCODE_SPACE))
       tetris.unpause();
     break;
+
   case Tetris::State::Running:
     // Check for pause, highest priority
-    if (!prev_keyboard[SDL_SCANCODE_SPACE] &&
-        curr_keyboard[SDL_SCANCODE_SPACE]) {
+    if (is_key_pressed(SDL_SCANCODE_SPACE)) {
       tetris.pause();
       break;
     }
 
     // Handle inputs which can be repeated
     for (auto &act : actions) {
-      if (!prev_keyboard[act.scancode] && curr_keyboard[act.scancode]) {
+      if (is_key_pressed(act.scancode)) {
         act.input_delay.reset();
         act.func.invoke_early();
-      } else if (prev_keyboard[act.scancode] && curr_keyboard[act.scancode]) {
+      } else if (is_key_held(act.scancode)) {
         act.input_delay.tick(delta);
         if (act.input_delay.has_set_off())
           act.func.tick(delta);
-      } else if (prev_keyboard[act.scancode] && !curr_keyboard[act.scancode]) {
+      } else if (is_key_released(act.scancode)) {
         act.input_delay.reset();
         act.func.reset();
       }
     }
 
     // Handle inputs with no repeat
-    if (curr_keyboard[SDL_SCANCODE_UP])
+    if (is_key_pressed(SDL_SCANCODE_UP))
       tetris.hold_active();
     break;
   }
 
+  // Keep a copy of the current keyboard for later
   std::copy_n(curr_keyboard, SDL_SCANCODE_COUNT, prev_keyboard);
+
   return AppResult::Continue;
 }
